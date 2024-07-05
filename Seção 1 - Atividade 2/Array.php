d773252023161844f0ee754ee559f6f3af431d5d
Gabriel Martins de Freitas <162491231+Ash2345meia78@users.noreply.github.com>
2024-07-04 23:33:29 -0300

    Delete Seção 1 - Atividade 2 directory

--- Seção 1 - Atividade 2/Array.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Declaração de um array com vários nomes
-$nome = ["Gabriel", "Manuel", "Miguel", "Daniel", "Rafael"];
-
-// Iterando sobre o array um loop foreach
-foreach ($nome as $nomes) {
-    // Imprimindo cada nome separadamente
-    echo "Nome: " . $nomes . "<br>";
-}
